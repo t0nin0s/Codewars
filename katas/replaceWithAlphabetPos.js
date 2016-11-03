e240b074811886d51c93e41df05322e6a97798e4
Antonio Sanchez Martinez <Antonio.Sanchez-Martinez@turner.com>
2016-11-03 16:10:09 +0000

    First Kata

--- katas/replaceWithAlphabetPos.js
+++ katas/replaceWithAlphabetPos.js
@@ -2,11 +2,10 @@ var assert = require('assert');
 
 var alphabetPosition = function (text) {
   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  return (text.replace(/ /g,"")
+  return (text.replace(/[^a-zA-Z]/g, "")
               .toLowerCase()
               .split('')
               .map(function(char){
-                console.log("char is: "+char);
                 return alphabet.indexOf(char)+1;
               }).join(' ')
   );
--- test/replaceWithAlphabetPos.js
+++ test/replaceWithAlphabetPos.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var alphabetPosition = require('../replaceWithAlphabetPos');
+var alphabetPosition = require('../katas/replaceWithAlphabetPos');
 
 describe('Replace With Alphabet Positions', function() {
   describe('1st set of tests', function() {
@@ -7,5 +7,12 @@ describe('Replace With Alphabet Positions', function() {
       assert.equal(alphabetPosition('   abc    abc abc    '), "1 2 3 1 2 3 1 2 3");
     });
 
+    it("The sunset sets at twelve o' clock.", function() {
+      assert.equal(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+    });
+
+    it('The narwhal bacons at midnight.', function() {
+      assert.equal(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+    });
   });
 });
