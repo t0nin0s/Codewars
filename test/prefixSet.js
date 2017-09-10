7fde6eb00a83186c465b20cb98b4a4d2971cef53
Toni Sanchez <toninos@gmail.com>
2017-09-10 19:05:42 +0100

    tests amend

--- test/prefixSet.js
+++ test/prefixSet.js
@@ -6,10 +6,10 @@ describe('prefix Set', function () {
     it('should return index 3', function () {
       assert.equal(prefixSet([2, 2, 1, 0, 1]), 3)
     })
-    it('should return index 3', function () {
+    it('should return index 6', function () {
       assert.equal(prefixSet([3, 7, 1, 13, 1, 3, 8, 13, 3, 7, 8]), 6)
     })
-    it('should return index 1', function () {
+    it('should return index 0', function () {
       assert.equal(prefixSet([1, 1, 1, 1, 1, 1, 1, '1', 1]), 0)
     })
   })
