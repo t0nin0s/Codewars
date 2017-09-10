c44fb264a3f314cd75895ea60ba913b95b6f63b2
Toni Sanchez <toninos@gmail.com>
2017-09-10 18:58:25 +0100

    more challenges

--- katas/balancedBrackets.js
+++ katas/balancedBrackets.js
@@ -1,5 +1,42 @@
 function balancedBrackets (data) {
+  let array = data.split('').reduce((array, curr, index) => {
+    if (isOpenBracket(curr)) {
+      array.push(curr)
+      return array
+    } else {
+      if (matches(array[array.length - 1], curr)) {
+        array.pop()
+      } else {
+        array.push(curr)
+      }
+      return array
+    }
+  }, [])
+  if (array.length > 0) {
+    return 'NO'
+  } else {
+    return 'YES'
+  }
+}
 
+function isOpenBracket (char) {
+  let str = '{[('
+  if (str.indexOf(char) > -1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function matches (open, close) {
+  let openingBracket = '{[('
+  let closingBrackets = '}])'
+
+  if (openingBracket.indexOf(open) === closingBrackets.indexOf(close)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = balancedBrackets
---
+++ test/balancedBrackets.js
@@ -0,0 +1,13 @@
+var assert = require('assert')
+var balancedBrackets = require('../katas/balancedBrackets')
+
+describe('Balanced Brackets', function () {
+  describe('1st set of tests', function () {
+    it('should return index YES', function () {
+      assert.equal(balancedBrackets('{[()]}'), 'YES')
+    })
+    it('should return index NO', function () {
+      assert.equal(balancedBrackets('{{]}}'), 'NO')
+    })
+  })
+})
