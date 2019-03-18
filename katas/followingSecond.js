1346f2aa214029e43eb83267e4a7dca12417ba2e
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2019-03-18 22:56:55 +0000

    Reviewing Following second

--- katas/followingSecond.js
+++ katas/followingSecond.js
@@ -1,7 +1,7 @@
 //Write a program that asks for a time under the form of three information
 //(hours, minutes, seconds).
 //The program calculates and shows the time one second after.
-//Incorrect inputs must be taken into account.
+//Incorrect inputs must be taken into account
 
 function followingSecond() {
   let second = prompt("Current second");
