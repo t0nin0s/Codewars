23197c938bddb99f332746b5a1888a6cd4a6d0ab
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2019-04-15 15:01:13 +0100

    strayNumber & tests

--- test/strayNumber.js
+++ test/strayNumber.js
@@ -1,14 +1,11 @@
 var assert = require("assert");
 var strayNumber = require("../katas/strayNumber");
 
-describe.only("stray number", function() {
+describe("stray number", function() {
   it("should return the unique number", function() {
     assert.equal(strayNumber([1, 1, 2, 1, 1]), 2);
   });
   it("should return negative unique number", function() {
     assert.equal(strayNumber([-2, -1, -1, -1, -1]), -2);
-  });
-  it("should not accept strings", function() {
-    assert.throws(strayNumber, Error, "Strings not valid");
   });
 });
