baa1282428467387f6ad7b30bd60c1b2ea2e6cc8
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2019-05-12 20:19:36 +0100

    tennisBalls kata & tests - used deepEqual on test

---
+++ katas/tennisBalls.js
@@ -0,0 +1,7 @@
+function tennisBalls(detritus) {
+  const balls = detritus.filter((item) => item === 58).reduce((acc, value) =>
+    total = acc + value, 0);
+  return { weight: balls };
+}
+
+module.exports = tennisBalls;
